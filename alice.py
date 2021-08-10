2d72700075d2be51b198979093fd7aa5100b4988
Ekawahyu Susilo <ekawahyu@yahoo.com>
2021-08-10 11:33:32 -0700

    hide credential using environment variables

--- alice.py
+++ alice.py
@@ -1,12 +1,13 @@
 from py123mqtt import Messaging
 from py123mqtt import Connection
+import os
 import threading
 
 def send_periodic_message():
     alice.send_message(bob, 'hello Bob!')
     threading.Timer(5, send_periodic_message).start()
 
-alice = Messaging(private_key='ff746ad94861f073d07196d6c901b45f', username='Old-Engine-6372', password='0CewA5a1')
+alice = Messaging(private_key=os.environ.get('PRIVATEKEY'), username=os.environ.get('USERNAME'), password=os.environ.get('PASSWORD'))
 alice.show_info()
 
 bob = Connection('Bob Lee', 'Silly-Headrest-2013')
--- bob.py
+++ bob.py
@@ -1,12 +1,13 @@
 from py123mqtt import Messaging
 from py123mqtt import Connection
+import os
 import threading
 
 def send_periodic_message():
     bob.send_message(alice, 'hello Alice!')
     threading.Timer(5, send_periodic_message).start()
 
-bob = Messaging(private_key='bb3f488769037d24358f8060111b76fd', username='Silly-Headrest-2013', password='jTeMIfTy')
+bob = Messaging(private_key=os.environ.get('PRIVATEKEY'), username=os.environ.get('USERNAME'), password=os.environ.get('PASSWORD'))
 bob.show_info()
 
 alice = Connection('Alice Wonder', 'Old-Engine-6372')
